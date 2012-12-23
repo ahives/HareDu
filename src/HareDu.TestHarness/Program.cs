e57641df7a95d02c5ff0628a3a4ead0961c9f3b0
Dominique Plante <dominique.plante@gmail.com>
2012-12-22 18:28:24 -0800

    refactoring - move code to output on all open channels to its own method

--- src/HareDu.TestHarness/Program.cs
+++ src/HareDu.TestHarness/Program.cs
@@ -17,21 +17,27 @@ namespace HareDu.TestHarness
             string password = Console.ReadLine();
 
             url = "http://localhost";
+            outputAllOpenChannels(url, username, password);
+        }
+
+        private static void outputAllOpenChannels(string url, string username, string password)
+        {
             var client = new HareDuClient(url, 55672, username, password);
             var requestTask = client.GetListOfAllOpenChannels();
-                                 var responseTask = requestTask.ContinueWith(x =>
-                                                   {
-                                                       var response = x.Result;
+            var responseTask = requestTask.ContinueWith(x =>
+                                                            {
+                                                                var response = x.Result;
 
-                                                       foreach (var channel in response.GetResponse<IEnumerable<Channel>>())
-                                                       {
-                                                           Console.WriteLine(channel.Name);
-                                                           Console.WriteLine(channel.Node);
-                                                       }
-                                                   });
+                                                                foreach (
+                                                                    var channel in response.GetResponse<IEnumerable<Channel>>())
+                                                                {
+                                                                    Console.WriteLine(channel.Name);
+                                                                    Console.WriteLine(channel.Node);
+                                                                }
+                                                            });
             responseTask.Wait();
-                                 //Task.Factory.StartNew(() => requestTask);
-                                 //Task.Factory.StartNew(() => responseTask);
+            //Task.Factory.StartNew(() => requestTask);
+            //Task.Factory.StartNew(() => responseTask);
             //.Result.Content.ReadAsAsync<IEnumerable<Channel>>().Result;
 
             //foreach (var channelInfo in channels)
