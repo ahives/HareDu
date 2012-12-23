8f6f6a12e76c877ad5d825fbc64b21d0d93a5df9
Dominique Plante <dominique.plante@gmail.com>
2012-12-22 18:16:53 -0800

    remove unused declaratives

--- src/HareDu.TestHarness/Program.cs
+++ src/HareDu.TestHarness/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Reflection;
-using System.Threading.Tasks;
 
 namespace HareDu.TestHarness
 {
