221981501d9ad47614497444f039d6fc4fe9a2b5
Albert L. Hives <albert.hives@relayhealth.com>
2013-02-23 16:28:56 -0800

    documentation in prep for v1.0 release

--- src/HareDu/Client/HareDuClient.cs
+++ src/HareDu/Client/HareDuClient.cs
@@ -20,6 +20,13 @@ namespace HareDu
 
     public interface HareDuClient
     {
+        /// <summary>
+        /// Creates a new instance of object implemented by T, which encapsulates a group of resources (e.g. Virtual Host, Exchange, Queue, User, etc.)
+        /// that are exposed by the RabbitMQ server via its REST API.
+        /// </summary>
+        /// <typeparam name="T">Interface that derives from base interface ResourceClient.</typeparam>
+        /// <param name="userCredentials">Username and password of valid user on a RabbitMQ server.</param>
+        /// <returns>An interface of resources available on a RabbitMQ server.</returns>
         T Factory<T>(Action<UserCredentials> userCredentials)
             where T : ResourceClient;
 
